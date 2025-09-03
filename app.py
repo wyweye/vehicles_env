0bf2463129e95b167f40b2a88bdf5f670337cf14
wyweye <wyweye@gmail.com>
2025-09-03 11:20:34 -0600

    subiendo repo

---
+++ app.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import plotly.graph_objects as go  # Importación de plotly.graph_objects como go
+import streamlit as st
+
+# Leer los datos del archivo CSV
+car_data = pd.read_csv(
+    'C:/Users/Lenovo/Python/vehicles_env/vehicles_us.csv')
+
+# Crear un botón en la aplicación Streamlit
+hist_button = st.button('Construir histograma')
+
+# Lógica a ejecutar cuando se hace clic en el botón
+if hist_button:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(data=[go.Histogram(x=car_data['odometer'])])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
