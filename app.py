ced6dc87c06171a453f9954e8730f1e74b47c8af
wyweye <wyweye@gmail.com>
2025-09-03 12:02:58 -0600

    nuevo archivo

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ car_data = pd.read_csv(
 
 # Crear un botón en la aplicación Streamlit
 hist_button = st.button('Construir histograma')
+disp_button = st.button('Construir grafica de dispersion')
 
 # Lógica a ejecutar cuando se hace clic en el botón
 if hist_button:
@@ -21,6 +22,22 @@ if hist_button:
 
     # Opcional: Puedes añadir un título al gráfico si lo deseas
     fig.update_layout(title_text='Distribución del Odómetro')
+
+    # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
+    # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
+    st.plotly_chart(fig, use_container_width=True)
+if disp_button:
+    # Escribir un mensaje en la aplicación
+    st.write(
+        'Creación de grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    # Crear un histograma utilizando plotly.graph_objects
+    # Se crea una figura vacía y luego se añade un rastro de histograma
+    fig = go.Figure(
+        data=[go.Scatter(x=car_data['odometer'], y=car_data['price'], mode='markers')])
+
+    # Opcional: Puedes añadir un título al gráfico si lo deseas
+    fig.update_layout(title_text='Relación entre Odómetro y Precio')
 
     # Mostrar el gráfico Plotly interactivo en la aplicación Streamlit
     # 'use_container_width=True' ajusta el ancho del gráfico al contenedor
